326f7312b1ef4eb155442b38ff8555dfb8c91ef1
Zipster23 <86753446+Zipster23@users.noreply.github.com>
2025-05-29 11:40:19 -0400

    Projectile prefab
    created projectile prefab

---
+++ Unity Car Crusaders/Assets/Scripts/Projectile.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10;
+    public GameObject projectilePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+    }
+}
